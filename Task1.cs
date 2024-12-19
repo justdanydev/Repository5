73342cf5b6ffb3eedebd0d93e03726675683ef53
JUSTDANYdev <bobrovnikovdanila@gmail.com>
2024-12-19 18:52:20 +0300

    Initial commit

---
+++ Task1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository5
+{
+
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Площадь круга: {circle.GetArea()}");
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.GetArea()}");
+        }
+    }
+}
